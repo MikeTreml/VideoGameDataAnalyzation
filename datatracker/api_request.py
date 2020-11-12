6b48c370717eb85e42f7214b7311741b8c29edbf
mike treml <miketreml@mac.com>
2020-11-12 11:26:42 -0600

    search_request created

--- datatracker/api_request.py
+++ datatracker/api_request.py
@@ -6,7 +6,16 @@ here = 'outside'
 def run_request():
     response = requests.get('https://api.dccresource.com/api/games')
     jsonobject = response.text
-    global here
-    here = json.loads(jsonobject, object_hook=lambda d: SimpleNamespace(**d))
-    return here[1]
+    fulldata = json.loads(jsonobject, object_hook=lambda d: SimpleNamespace(**d))
+    result = list(filter(lambda x: x.year == 1996, fulldata))
+    return result
    # return json.loads(jsonobject, object_hook=lambda d: SimpleNamespace(**d))
+
+def search_request(searchstring):
+    response = requests.get('https://api.dccresource.com/api/games')
+    jsonobject = response.text
+    fulldata = json.loads(jsonobject, object_hook=lambda d: SimpleNamespace(**d))
+    result = list(filter(lambda x: searchstring.lower() in x.name.lower(), fulldata))
+    return result
+    #for n in result:
+        #print(n.name)
