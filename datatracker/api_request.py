55b070ab282d3f84226fbb13d988b6afe56411b4
mike treml <miketreml@mac.com>
2020-11-13 23:15:30 -0600

    moved to one api call for all methods

--- datatracker/api_request.py
+++ datatracker/api_request.py
@@ -1,11 +1,10 @@
 from typing import Dict, Any, Union
-
 import requests
 import json
 from types import SimpleNamespace
 
-# api request for whole json get. list wist dot notation.
-
+# one api request on page load
+api_request = requests.get('https://api.dccresource.com/api/games')
 consoledict = {"2600": {"naSales": 0, "euSales": 0, "jpSales": 0, "otherSales": 0, "globalSales": 0},
                "3DO": {"naSales": 0, "euSales": 0, "jpSales": 0, "otherSales": 0, "globalSales": 0},
                "3DS": {"naSales": 0, "euSales": 0, "jpSales": 0, "otherSales": 0, "globalSales": 0},
@@ -40,17 +39,56 @@ consoledict = {"2600": {"naSales": 0, "euSales": 0, "jpSales": 0, "otherSales": 
 
 # def giving a range from 2013 to 2017
 def request_default():
-    response = requests.get('https://api.dccresource.com/api/games')
+    response = api_request
     jsonobject = response.text
-    results = []
-    result = []
     fulldata = json.loads(jsonobject, object_hook=lambda d: SimpleNamespace(**d))
     resultyear = list(filter(lambda x: str(x.year) == '2013' or
                                    str(x.year) == '2014' or
                                    str(x.year) == '2015' or
                                    str(x.year) == '2016' or
                                    str(x.year) == '2017', fulldata))
-    for item in resultyear:
+
+    return proccess_filter(resultyear)
+
+# select year from drop down
+def request_games_year(year):
+    response = api_request
+    jsonobject = response.text
+    fulldata = json.loads(jsonobject, object_hook=lambda d: SimpleNamespace(**d))
+    result = list(filter(lambda x: str(x.year) == year, fulldata))
+
+    return result
+
+# search function by string
+def request_search_string(searchstring):
+    response = api_request
+    jsonobject = response.text
+    fulldata = json.loads(jsonobject, object_hook=lambda d: SimpleNamespace(**d))
+    result = list(filter(lambda x: searchstring.lower() in x.name.lower(), fulldata))
+
+    return result
+
+# select year to lst the consoles that sold the number of games that year
+def request_console_year(year):
+    response = api_request
+    jsonobject = response.text
+    fulldata = json.loads(jsonobject, object_hook=lambda d: SimpleNamespace(**d))
+    resultyear = list(filter(lambda x: str(x.year) == year, fulldata))
+
+    return proccess_filter(resultyear)
+
+
+def request_console_game(name):
+    response = api_request
+    jsonobject = response.text
+    fulldata = json.loads(jsonobject, object_hook=lambda d: SimpleNamespace(**d))
+    resultyear = list(filter(lambda x: str(x.name) == name, fulldata))
+
+    return proccess_filter(resultyear)
+
+def proccess_filter(dict):
+    results = []
+    for item in dict:
         consoledict[item.platform]["naSales"] += item.naSales
         consoledict[item.platform]["euSales"] += item.euSales
         consoledict[item.platform]["jpSales"] += item.jpSales
@@ -62,62 +100,4 @@ def request_default():
                 result = [console, sales["naSales"], sales["euSales"], sales["jpSales"], sales["otherSales"], sales["globalSales"]]
                 results.append(result)
 
-    return results
-
-# select year from drop down
-def request_games_year(year):
-    response = requests.get('https://api.dccresource.com/api/games')
-    jsonobject = response.text
-    fulldata = json.loads(jsonobject, object_hook=lambda d: SimpleNamespace(**d))
-    result = list(filter(lambda x: str(x.year) == year, fulldata))
-    return result
-
-# search function by string
-def request_search_string(searchstring):
-    response = requests.get('https://api.dccresource.com/api/games')
-    jsonobject = response.text
-    fulldata = json.loads(jsonobject, object_hook=lambda d: SimpleNamespace(**d))
-    result = list(filter(lambda x: searchstring.lower() in x.name.lower(), fulldata))
-    return result
-
-# select year to lst the consoles that sold the number of games that year
-def request_console_year(year):
-    response = requests.get('https://api.dccresource.com/api/games')
-    jsonobject = response.text
-    fulldata = json.loads(jsonobject, object_hook=lambda d: SimpleNamespace(**d))
-    resultyear = list(filter(lambda x: str(x.year) == year, fulldata))
-    result = {}
-
-    for item in resultyear:
-        consoledict[item.platform]["naSales"] += item.naSales
-        consoledict[item.platform]["euSales"] += item.euSales
-        consoledict[item.platform]["jpSales"] += item.jpSales
-        consoledict[item.platform]["otherSales"] += item.otherSales
-        consoledict[item.platform]["globalSales"] += item.globalSales
-
-    for console, sales in consoledict.items():
-        for key in sales:
-            if sales["globalSales"] > 0:
-                result[console] = sales
-    return result
-
-
-def request_console_game(name):
-    response = requests.get('https://api.dccresource.com/api/games')
-    jsonobject = response.text
-    fulldata = json.loads(jsonobject, object_hook=lambda d: SimpleNamespace(**d))
-    resultyear = list(filter(lambda x: str(x.name) == name, fulldata))
-    result = {}
-
-    for item in resultyear:
-        consoledict[item.platform]["naSales"] += item.naSales
-        consoledict[item.platform]["euSales"] += item.euSales
-        consoledict[item.platform]["jpSales"] += item.jpSales
-        consoledict[item.platform]["otherSales"] += item.otherSales
-        consoledict[item.platform]["globalSales"] += item.globalSales
-
-    for console, sales in consoledict.items():
-        for key in sales:
-            if sales["globalSales"] > 0:
-                result[console] = sales
-    return result
+    return(results)
